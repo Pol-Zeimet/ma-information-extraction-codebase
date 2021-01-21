>>Fixed: Fehler, bei dem Modelle doppel compiliert wurden und falschen loss berechnen wollten >>Changed: Architektur der Graphennetze aufgeräumet >>Changed: Prediction auf Softmax gibt jetzt vorhersagen in batchformat zurück, für einheitliche Ausgabenüber Modelle hinweg zu bewaren >>Changed: MlFlow Logging für Baseline speichert werte jetzt nach train/eval/test >>Changed: Experiment Scripte erweitert >>Added neues Experiment Script für CRFv2

graph_networks/src/models/bert_model.py
@@ -37,7 +37,6 @@ class BertConfig(Config):
         self.overwrite_output_dir = overwrite_output_dir
 
 
-
 class BertModel:
     def __init__(self, config: BertConfig, training_args, data_args, model_args):
         self.config = config
@@ -54,6 +53,7 @@ class BertModel:
         self.model: AutoModelForTokenClassification = None
         self.tokenizer: PreTrainedTokenizerBase = None
         self.trainer: Trainer = None
+        self.trainer_state = None
         self._create_model_architecture()
 
     def get_details(self) -> Dict[str, Any]:
@@ -69,6 +69,7 @@ class BertModel:
         self.tokenizer, self.model = Bert.create(self.data_args, self.model_args, self.config.num_classes)
 
     def train(self, data):
+        self.trainer_state = 'train'
         data_collator = DataCollatorForTokenClassification(self.tokenizer)
         self.trainer = Trainer(
             model=self.model,
@@ -86,9 +87,11 @@ class BertModel:
                            else None)
 
     def evaluate(self):
+        self.trainer_state = 'evaluate'
         return self.trainer.evaluate()
 
     def predict(self, datasets):
+        self.trainer_state = 'test'
         test_dataset = datasets["test"]
         predictions, labels, metrics = self.trainer.predict(test_dataset)
         predictions = np.argmax(predictions, axis=2)
@@ -118,10 +121,10 @@ class BertModel:
         recall = recall_score(true_labels, true_predictions)
         f1 = f1_score(true_labels, true_predictions)
 
-        mlflow.log_metric("eval_accuracy", acc)
-        mlflow.log_metric("eval_f1", f1)
-        mlflow.log_metric("eval_recall", recall)
-        mlflow.log_metric("eval_precision", precision)
+        mlflow.log_metric(self.trainer_state + "_accuracy", acc)
+        mlflow.log_metric(self.trainer_state + "_f1", f1)
+        mlflow.log_metric(self.trainer_state + "_recall", recall)
+        mlflow.log_metric(self.trainer_state + "_precision", precision)
 
         return {
             "accuracy_score": acc,

graph_networks/src/models/graph_model.py
@@ -103,8 +103,6 @@ class GraphModel:
                                                   adam
                                                   )
 
-            self.model.compile(optimizer=adam, loss="categorical_crossentropy", metrics=["accuracy"])
-
         elif self.model_type == "CRF":
             self.model = GraphModelCRF.create(self.config.node_count,
                                               self.config.edge_count,
@@ -118,7 +116,6 @@ class GraphModel:
                                               self.config.bilstm_units,
                                               adam
                                               )
-            self.model.compile(optimizer=adam, metrics=["accuracy"])
 
         elif self.model_type == "CRFv2":
             self.model = GraphModelCRFv2.create(self.config.node_count,
@@ -138,7 +135,7 @@ class GraphModel:
         return self.model.fit(verbose=1, x=train_generator, epochs=15, steps_per_epoch=20)
 
     def train_on_single_batch(self, inputs, targets):
-        return self.model.train_on_batch(inputs, [targets])
+        return self.model.train_on_batch(inputs, targets)
 
     def predict(self, x) -> (np.ndarray, np.ndarray):
         output = self.model.predict(x)
@@ -147,6 +144,7 @@ class GraphModel:
             graph_embeddings = output[1]
             predictions = tf_one_hot(tf_argmax(predictions, axis=2), depth=self.config.num_classes)
             predictions = [np.where(labels == 1)[0][0] for prediction in predictions for labels in prediction]
+            predictions = np.reshape(predictions, (self.config.batch_size, self.config.node_count))
         else:
             predictions = output[0][0]
             graph_embeddings = output[1]