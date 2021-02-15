>>Fixed: Zugriff auf zu loggenden Loss ist unterschiedlich bei Softmax und CRF

graph_networks/src/models/graph_model.py
@@ -133,7 +133,12 @@ class GraphModel:
         return self.model.fit(verbose=1, x=train_generator, epochs=15, steps_per_epoch=20)
 
     def train_on_single_batch(self, inputs, targets):
-        return self.model.train_on_batch(inputs, targets)
+        result = self.model.train_on_batch(inputs, targets)
+        if self.model_type == "Softmax":
+            loss = result[0]
+        else:
+            loss = result
+        return loss
 
     def predict(self, x) -> (np.ndarray, np.ndarray):
         output = self.model.predict(x)